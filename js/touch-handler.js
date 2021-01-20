5fa480d61465ce8ef0b8ad43ab20ac5a176f9140
yichen li <yichenlilyc@gmail.com>
2021-01-19 22:52:17 -0800

    touchtest

--- js/touch-handler.js
+++ js/touch-handler.js
@@ -1,11 +1,45 @@
 AFRAME.registerComponent('touch-handler', {
 
+  
+
     init: function() {
-        const animatedMarker = document.querySelector("#animated-marker");
-        const aEntity = document.querySelector("#thebox");
-        console.log("start touch");
+        this.isVisible = false;
+        
+        console.log("start touch,update");
+        console.log(this.el);
         // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, target){
-            console.log(ev.detail.intersection.point);
+        this.el.sceneEl.addEventListener("markerFound", (evt) => {
+        this.isVisible = true;
+        console.log(this.isVisible);
+        this.el.addEventListener('click', this.clickEvent);
         });
-}});
+    
+        this.el.sceneEl.addEventListener("markerLost", (evt) => {
+        this.isVisible = false;
+        console.log("false");
+        //this.el.removeEventListener('click', this.clickEvent);
+        });
+        
+        
+    },
+
+    update: function(){
+        if (this.isVisible) {
+            console.log("updatetrue");
+          } else {
+            console.log("updatefalse");
+          }
+    },
+
+    remove: function(){
+        this.el.removeEventListener('click',this.clickEvent);
+    },
+
+    clickEvent(event){
+        
+        console.log("getclick");
+        console.log(event.detail.intersection.point);
+        
+        
+    }
+});
